2b9f66998d2f893a741050adb86a19fd7c72a1f2
Katiasfihi <katia.sfihi@gmail.com>
2021-09-28 23:25:41 +0100

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import React from 'react'
+
+import logo from '../photos/pink.jpg'; 
+
+function About() {
+  return (
+ 
+      <img src={logo} alt="Logo" />
+
+    // </div>
+  );
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+// import './App.css';
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+    <h1>Katia & Carol</h1>
+    <ul className='nav-wrapper'>    
+        <li className='nav-item'>Schedule</li> 
+        <li className='nav-item'>Information</li>
+        <li className='nav-item'>RSVP</li>
+    </ul>
+
+
+    </div>
+  );
+}
+
+export default Header;
